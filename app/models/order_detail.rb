e38997bfe032351f34a8c5634ae0ce75a2746124
dainagak <dainagak@gmail.com>
2021-11-21 16:24:48 +0000

    [Fix]layout調整4

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,7 @@
 class OrderDetail < ApplicationRecord
-  belongs_to :cart_items
-  belongs_to :items
-
+  belongs_to :cart_item
+  belongs_to :item
+  belongs_to :order
     enum making_status: { "着手不可": 0, "製作待ち": 1, "製作中": 2, "製作完了": 3 }
 
 end
